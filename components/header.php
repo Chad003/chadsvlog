438025b40369f61d7bbb5e2e7164347451b7e182
chad.jamilla <chadeujamilla@gmail.com>
2025-10-23 14:10:45 +0800

    Added google tags

--- components/header.php
+++ components/header.php
@@ -6,3 +6,13 @@
     integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://cdn.tailwindcss.com"></script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BCB1SH2HFS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BCB1SH2HFS');
+</script>
