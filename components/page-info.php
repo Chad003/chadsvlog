5786fe4dfc7329d66f8d2bcd1d4f0eaf7f614b6f
chad.jamilla <chadeujamilla@gmail.com>
2025-10-27 15:06:47 +0800

    Added social media icons

--- components/page-info.php
+++ components/page-info.php
@@ -1,4 +1,4 @@
-<footer class="text-center py-4 mt-10 rounded-lg w-full mb-16 md:mb-0 text-black-300">
+<!-- <footer class="text-center py-4 mt-10 rounded-lg w-full mb-16 md:mb-0 text-black-300">
     <p class="text-sm sm:text-base">
         Made with
         <span class="text-blue-400 font-semibold hover:text-blue-300 transition-colors">Tailwind CSS</span>
@@ -7,6 +7,36 @@
         <span class="text-red-400 font-bold hover:text-yellow-300 transition-colors">PHP</span>
     </p>
     <div class="space-y-3 text-gray-700">
+        <p class="text-gray-600 italic text-center text-sm">
+            “Code hard, fly high, and explore endlessly.”
+        </p>
+    </div>
+</footer> -->
+
+<footer class="text-center py-6 mt-10 rounded-lg w-full mb-16 md:mb-0 text-[#3d3d2f] bg-[#f4f1ec]">
+    <p class="text-sm sm:text-base">
+        Made with
+        <span class="text-blue-400 font-semibold hover:text-blue-300 transition-colors">Tailwind CSS</span> —
+        Powered by
+        <span class="text-red-400 font-bold hover:text-yellow-300 transition-colors">PHP</span>
+    </p>
+
+    <div class="mt-4 flex justify-center gap-6">
+        <a href="https://facebook.com/chadsVlog" target="_blank"
+            class="text-[#727b46] hover:text-[#88785f] transition-colors text-2xl">
+            <i class="fa-brands fa-facebook"></i>
+        </a>
+        <a href="https://www.tiktok.com/@chadsvlog" target="_blank"
+            class="text-[#727b46] hover:text-[#88785f] transition-colors text-2xl">
+            <i class="fa-brands fa-tiktok"></i>
+        </a>
+        <a href="https://www.instagram.com/chdgrphcs/" target="_blank"
+            class="text-[#727b46] hover:text-[#88785f] transition-colors text-2xl">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+    </div>
+
+    <div class="space-y-3 text-gray-700 mt-4">
         <p class="text-gray-600 italic text-center text-sm">
             “Code hard, fly high, and explore endlessly.”
         </p>
