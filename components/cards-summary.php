371b71fb2ab24693a467a59e1a1d3f0abb6c16f0
chad.jamilla <chadeujamilla@gmail.com>
2026-02-23 20:02:39 +0800

    Add months in work experience

--- components/cards-summary.php
+++ components/cards-summary.php
@@ -1,6 +1,8 @@
 <?php
+date_default_timezone_set('Asia/Manila');
+
 $LIFE_PERCENT = round(( (new DateTime("2000-06-03"))->diff(new DateTime())->days / (new DateTime("2000-06-03"))->diff(new DateTime("2080-06-03"))->days ) * 100, 2) . "%";
-$DEVELOPER_EXPERINCE = (new DateTime("2022-09-13"))->diff(new DateTime())->y . " years | " . (new DateTime("2022-09-13"))->diff(new DateTime())->d . " days";
+$DEVELOPER_EXPERINCE = (new DateTime("2022-09-13"))->diff(new DateTime())->y . " years | ". (new DateTime("2022-09-13"))->diff(new DateTime())->m . " months | " . (new DateTime("2022-09-13"))->diff(new DateTime())->d . " days";
 $cards = [
     ["icon" => "fa-code", "title" => "Work Experience", "value" => $DEVELOPER_EXPERINCE],
     ["icon" => "fa-heart", "title" => "Life Progress / 80 years", "value" => $LIFE_PERCENT],
@@ -17,7 +19,7 @@ $cards = [
                 <i class="fa-solid <?= $card['icon']; ?> text-[#727b46] text-2xl"></i>
                 <div class="text-left truncate">
                     <h3 class="text-[#727b46] text-base font-semibold leading-snug truncate"><?= $card['title']; ?></h3>
-                    <p class="text-xl font-bold text-[#3d3d2f]"><?= $card['value']; ?></p>
+                    <p class="text-sm font-bold text-[#3d3d2f]"><?= $card['value']; ?></p>
                 </div>
             </div>
         <?php endforeach; ?>
@@ -31,7 +33,7 @@ $cards = [
                     <h3 class="text-[#727b46] text-base md:text-lg font-semibold leading-snug truncate">
                         <?= $card['title']; ?>
                     </h3>
-                    <p class="text-lg md:text-2xl font-bold text-[#3d3d2f]"><?= $card['value']; ?></p>
+                    <p class="text-lg md:text-1xl font-bold text-[#3d3d2f]"><?= $card['value']; ?></p>
                 </div>
             </div>
         <?php endforeach; ?>
